d6c5ff834e2188acf1da0cffafc72fadefa46118
Tyisha <tlwilliams895@gmail.com>
2019-10-17 13:18:19 -0400

    update db

--- .history/main_20191017124803.py
+++ .history/main_20191017124803.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
-db = SQLAlchemy(app)
+# db = SQLALCHEMY(app)
 
 
 class Task(db.Model):
--- .history/main_20191017124942.py
+++ .history/main_20191017124942.py
@@ -1,20 +1,21 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
-db = SQLAlchemy(app)
+# db = SQLALCHEMY(app)
 
 
-class Task(db.Model):
+# class Task(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(120))
 
-    def __init__(self, name):
-        self.name = name
+#     def __init__(self, name):
+#         self.name = name
 
 
 tasks = []
